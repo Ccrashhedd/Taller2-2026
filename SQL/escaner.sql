abc2f32379d51c5e1c93058f43d81fefc8c3c657
Ccrashh <SMoralesF1403@gmail.com>
2026-04-07 16:04:08 -0500

    raaahhhh

---
+++ SQL/escaner.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS escaner
+USE escaner
+
+CREATE TABLE IF NOT EXISTS escaner (
+    idProducto INT AUTO_INCREMENT PRIMARY KEY,
+    codigo_barras BIGINT NOT NULL,
+    nombre VARCHAR (150) NOT NULL,
+    precio DECIMAL (20,4) NOT NULL,
+    cantidad INT NOT NULL,
+    categoria VARCHAR (50) NOT NULL
+);
